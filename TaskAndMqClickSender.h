988d2a423717ebe6db604b963c33676bf0ea160e
work1 <work1>
2025-03-14 17:40:17 +0800

    first commit

---
+++ TaskAndMqClickSender.h
@@ -0,0 +1,55 @@
+#pragma once
+ 
+#include "resource.h"
+#include "batch_login.h"
+#undef max
+#undef min
+#include "with_mqtt.h"
+#include "MsgParse.h"
+
+std::wstring stringToWstring(const std::string& str) {
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &wstr[0], size_needed);
+    return wstr;
+}
+
+std::string WcharToString(const wchar_t* wstr)
+{
+    // 获取转换后的字符数
+    int len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+
+    // 创建一个足够大的 buffer 来存放转换后的字符串
+    char* str = new char[len];
+
+    // 执行转换
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+
+    // 返回转换后的 std::string
+    std::string result(str);
+
+    delete[] str;  // 记得释放内存
+    return result;
+}
+
+
+// 获取控件文本的函数
+std::wstring GetEditText(HWND hDlg, int controlID) {
+    wchar_t szText[256];  // 缓冲区
+    GetWindowText(GetDlgItem(hDlg, controlID), szText, sizeof(szText) / sizeof(wchar_t));
+    return std::wstring(szText);
+}
+
+// 工具函数：将 TCHAR 转为 std::string
+std::string TCHARToString(const TCHAR* buffer) {
+    std::string msg;
+#ifdef UNICODE
+    // 如果是 Unicode，转换为 std::wstring 再转换为 std::string
+    std::wstring wstr(buffer);
+    msg.assign(wstr.begin(), wstr.end());
+#else
+    // 如果是 ANSI，直接赋值
+    msg = buffer;
+#endif
+    return msg;
+}
---
+++ resource.h
@@ -0,0 +1,36 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 TaskAndMqClick.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_TASKANDMQCLICK_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_TASKANDMQCLICK              107
+#define IDI_SMALL                       108
+#define IDC_TASKANDMQCLICK              109
+#define IDR_MAINFRAME                   128
+#define IDD_LOGINSBOX                   129
+#define ID_LOGINS_OK                    1000
+#define IDC_LOGIN_EXE_EDIT1             1001
+#define IDC_LOGIN_ACCOUNTS_EDIT2        1002
+#define ID_32771                        32771
+#define IDM_LOGINS                      32772
+#define ID_32774                        32774
+#define ID_REMOTE_CTRL_RUN              32775
+#define ID_REMOTE_CTRL_STOP             32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
